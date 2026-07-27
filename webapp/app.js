4aded7c768b2edb0bf8a50627a65372c6b165943
aslan-ux <aslansarimbet7@gmail.com>
2026-07-27 20:32:17 +0500

    Sync webapp/app.js: банк лого eager (реальный prod-путь)

--- webapp/app.js
+++ webapp/app.js
@@ -703,7 +703,7 @@ function finRenderBanks() {
   box.innerHTML = BANKS.map((b, i) => {
     const ini = b.n === 'Қолма-қол' ? '₸' : b.n === 'Басқа' ? '•' : b.n.slice(0, 2);
     const logo = b.d
-      ? '<img src="https://www.google.com/s2/favicons?domain=' + b.d + '&sz=128" alt="" loading="lazy" onerror="if(!this.dataset.f){this.dataset.f=1;this.src=\'https://icons.duckduckgo.com/ip3/' + b.d + '.ico\'}else{this.remove()}"/>'
+      ? '<img src="https://www.google.com/s2/favicons?domain=' + b.d + '&sz=128" alt="" loading="eager" decoding="async" onerror="if(!this.dataset.f){this.dataset.f=1;this.src=\'https://icons.duckduckgo.com/ip3/' + b.d + '.ico\'}else{this.remove()}"/>'
       : '';
     return '<div class="fin-bank' + (i === 0 ? ' sel' : '') + '" data-n="' + b.n + '"><div class="fin-bank-ic" style="color:' + b.c + '"><span>' + ini + '</span>' + logo + '</div><div class="fin-bank-n">' + b.n + '</div></div>';
   }).join('');
